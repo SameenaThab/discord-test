1d24f608f1eb11707bb3d0a6baf66e329c8e4bfc
sameena thabassum <sameenathabassum@sameenas-MacBook-Pro.local>
2022-08-30 15:18:41 -0700

    remove unused imports

--- fullstack-test/src/components/ReactionCreator.jsx
+++ fullstack-test/src/components/ReactionCreator.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactionIcon from "./ReactionIcon";
 import { emojiReactions } from "../constants";
+import styles from "./ReactionCreator.module.css";
 
 const  ReactionCreator = ({ messageId }) => {
     return (
-        Object.values(emojiReactions).map((emojiReaction) => (
-            <ReactionIcon messageId={messageId} emoji={emojiReaction}/>
-        ))
+        <div key={messageId} className={styles.reactions}>
+            {
+            Object.values(emojiReactions).map((emojiReaction) => (
+                <ReactionIcon messageId={messageId} emoji={emojiReaction}/>
+                ))
+            }
+        </div>
     );
 };
 
--- fullstack-test/src/components/ReactionIcon.jsx
+++ fullstack-test/src/components/ReactionIcon.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { createReaction, deleteReaction } from "../actions";
 import styles from "./ReactionIcon.module.css";
 import { useReactionStore } from "../stores/reactions";
